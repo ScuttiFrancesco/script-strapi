2b47b564a2c8d222b12de3bfb5d1bce26b494525
Francesco Scutti <francesco.scutti@intellitronika.com>
2026-05-27 17:24:16 +0200

    replace path da sito a strapi

--- src/script_strapi/main.py
+++ src/script_strapi/main.py
@@ -9,7 +9,7 @@ import os
 import time
 from strapi_fields import strapi_fields as sf
 from dotenv import load_dotenv
-from strapi_service import insert
+from strapi_service import insert, replace_and_update, retrieve_data
 from deserialize_excel import deserialize_excel, create_strapi_object
 from strapi_obj import *
 
@@ -27,7 +27,7 @@ if collection_name not in sf:
     exit(1)
 
 def main() -> None:   
-        df = deserialize_excel(collection_name)
+       """  df = deserialize_excel(collection_name)
         inseriti = 0
         falliti = 0
 
@@ -44,6 +44,10 @@ def main() -> None:
                     falliti += 1
                 time.sleep(1)
         logger.info(f"Record inseriti: {inseriti}, Record falliti: {falliti}")
-
+    """
+       retrieved_data = retrieve_data(collection_name, "media-e-comunicazione/rassegna-dellarma/la-rassegna")
+       for item in retrieved_data:
+           replace_and_update(collection_name, item['documentId'], item, "media---comunicazione/rassegna-dell-arma", "media-e-comunicazione/rassegna-dellarma")
+       
 if __name__ == "__main__":
     main()
--- src/script_strapi/strapi_service.py
+++ src/script_strapi/strapi_service.py
@@ -1,3 +1,5 @@
+import json
+
 import requests as req
 import urllib3
 import os
@@ -25,3 +27,68 @@ def insert(collection_name: str, data: dict) :
     except req.RequestException as e:
         print(f"Error inserting data into {collection_name}: {e.response.text}")
         return False
+    
+def retrieve_data(collection_name: str, section: str) -> list:
+    path = strapi_url + collection_name + f'?filters[url_addizionali][path][$contains]={section}&pLevel'
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    }
+    try:
+        response = req.get(path, headers=headers, verify=ssl_verify)
+        response.raise_for_status()
+        return response.json().get("data", [])
+    except req.RequestException as e:
+        print(f"Error retrieving data from {collection_name}: {e.response.text}")
+        return []
+    
+def replace_and_update(collection_name: str, id: int, data: dict, old: str, new: str) -> bool:
+    path = strapi_url + collection_name + f'/{id}'
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    }
+    try:
+        blocco_centrale = data.get("blocco_centrale", [])
+        if len(blocco_centrale) > 0 :
+            for contenuto in blocco_centrale:
+                if 'id' in contenuto:
+                    del contenuto['id']
+                if contenuto['__component'] == 'shared.contenuto':
+                    contenuto['editor'] = replace(contenuto['editor'], old, new)
+        spalla_destra = data.get("spalla_destra", [])
+        if len(spalla_destra) > 0 :
+            for contenuto in spalla_destra:
+                if 'id' in contenuto:
+                    del contenuto['id']
+                if contenuto['__component'] == 'shared.contenuto':
+                    contenuto['editor'] = replace(contenuto['editor'], old, new)
+        data["blocco_centrale"] = blocco_centrale
+        data["spalla_destra"] = spalla_destra
+        if 'id' in data:
+            del data['id']
+        if 'documentId' in data:
+            del data['documentId']
+        if 'documentId' in data:
+            del data['documentId']
+        if 'createdAt' in data:
+            del data['createdAt']
+        if 'updatedAt' in data:
+            del data['updatedAt']
+        if 'url_addizionali' in data:
+            del data['url_addizionali']
+        if 'pagina' in data:
+            del data['pagina']
+        if 'publishedAt' in data:
+            del data['publishedAt']
+        print (f"Data to update for ID {id}: {json.dumps(data, indent=2)}")
+        response = req.put(path, json={"data": data}, headers=headers, verify=ssl_verify)
+        response.raise_for_status()
+        print(f"Data updated successfully for ID {id}")
+        return True
+    except req.RequestException as e:
+        print(f"Error updating data in {collection_name} for ID {id}: {e.response.text}")
+        return False
+
+def replace(text: str, old: str, new: str) -> str:
+    return text.replace(old, new) if text else text
